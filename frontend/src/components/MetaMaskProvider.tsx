22c5eaa2dcd03e5af0f2b47bca458938ea2f6c02
lewwcom <lewwcom@outlook.com>
2022-07-21 23:59:22 +0000

    Switch from web3.js to ethers

--- frontend/src/components/MetaMaskProvider.tsx
+++ frontend/src/components/MetaMaskProvider.tsx
@@ -1,33 +1,32 @@
+import { ethers } from "ethers";
 import React, { ReactElement, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Web3 from "web3";
 
 interface MetaMask {
   account: string;
   accountShort: string;
-  isIssuer: boolean;
+  getBalance: () => Promise<number>;
   connectToMetaMask: () => void;
 }
 
 const getShortAccount = (account: string) =>
   account.substring(0, 5) + "..." + account.substring(account.length - 4);
 
-const web3 = new Web3(Web3.givenProvider);
+// TODO: What if metamask is not installed
+const provider = new ethers.providers.Web3Provider(
+  (window as any).ethereum,
+  97
+);
 
 const MetaMaskContext = React.createContext<MetaMask>({
   account: "",
-  get accountShort() {
-    return getShortAccount(this.account);
-  },
-  isIssuer: false,
-  connectToMetaMask: function () {
-    web3.eth.requestAccounts((_error, account) => (this.account = account[0]));
-  },
+  accountShort: "",
+  getBalance: () => Promise.resolve(0),
+  connectToMetaMask: () => {},
 });
 
 const MetaMaskProvider = ({ children }: { children: ReactElement }) => {
   const [account, setAccount] = useState("");
-  const [isIssuer, setIsIssuer] = useState(false);
   const navigate = useNavigate();
 
   const updateAccount = (
@@ -36,7 +35,6 @@ const MetaMaskProvider = ({ children }: { children: ReactElement }) => {
   ) => {
     if (account !== undefined) {
       setAccount(account);
-      setIsIssuer(true);
       if (navigateToCollections) {
         navigate("/collections");
       }
@@ -45,18 +43,22 @@ const MetaMaskProvider = ({ children }: { children: ReactElement }) => {
     }
   };
 
-  web3.eth.getAccounts((_error, account) => updateAccount(account[0]));
+  provider
+    .send("eth_accounts", [])
+    .then((accounts) => updateAccount(accounts[0]));
 
   const context: MetaMask = {
     account: account,
     get accountShort() {
       return getShortAccount(this.account);
     },
-    isIssuer: isIssuer,
+    getBalance: async function () {
+      return (await provider.getBalance(this.account)).toNumber();
+    },
     connectToMetaMask: () =>
-      web3.eth.requestAccounts((_error, account) =>
-        updateAccount(account[0], true)
-      ),
+      provider
+        .send("eth_requestAccounts", [])
+        .then((accounts) => updateAccount(accounts[0], true)),
   };
 
   return <MetaMaskContext.Provider value={context} children={children} />;
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Logo } from "assets/global.svg";
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useMetaMask } from "./MetaMaskProvider";
 
@@ -26,56 +27,65 @@ const NavBar = ({ transparent }: { transparent?: boolean }) => {
   );
 };
 
-const ConnectedNav = () => {
+const ConnectedNav = () => (
+  <>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbar-collapse"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbar-collapse">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <NavLink
+            to="/collections"
+            className={`nav-link fw-semibold ${({
+              isActive,
+            }: {
+              isActive: boolean;
+            }) => (isActive ? "active" : "")}`}
+          >
+            Collections
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/wallet"
+            className={`nav-link fw-semibold ${({
+              isActive,
+            }: {
+              isActive: boolean;
+            }) => (isActive ? "active" : "")}`}
+          >
+            Wallet
+          </NavLink>
+        </li>
+      </ul>
+      <hr className="border-light d-block d-sm-none" />
+      <AccountInfo />
+    </div>
+  </>
+);
+
+const AccountInfo = () => {
+  const [balance, setBalance] = useState(0);
   const metaMask = useMetaMask();
+
+  // TODO: handle big value
+  metaMask.getBalance().then((value) => setBalance(value));
+
   return (
-    <>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbar-collapse"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbar-collapse">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <NavLink
-              to="/collections"
-              className={`nav-link fw-semibold ${({
-                isActive,
-              }: {
-                isActive: boolean;
-              }) => (isActive ? "active" : "")}`}
-            >
-              Certificates
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/wallet"
-              className={`nav-link fw-semibold ${({
-                isActive,
-              }: {
-                isActive: boolean;
-              }) => (isActive ? "active" : "")}`}
-            >
-              Wallet
-            </NavLink>
-          </li>
-        </ul>
-        <hr className="border-light d-block d-sm-none" />
-        <div className="input-group w-auto ms-sm-2">
-          <span className="input-group-text text-light bg-transparent font-monospace">
-            {metaMask.accountShort}
-          </span>
-          <span className="input-group-text">
-            <strong>50</strong>&nbsp;USDT
-          </span>
-        </div>
-      </div>
-    </>
+    <div className="input-group w-auto ms-sm-2">
+      <span className="input-group-text text-light bg-transparent font-monospace">
+        {metaMask.accountShort}
+      </span>
+      <span className="input-group-text">
+        <strong>{balance}</strong>&nbsp;USDT
+      </span>
+    </div>
   );
 };
 
